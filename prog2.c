3c57b89c8988492dea47672288386d1b19abd8a3
prishaa <prishapuj7@gmail.com>
2025-11-15 09:57:43 +0530

    Add files via upload

--- prog2.c
+++ prog2.c
@@ -1,48 +1,25 @@
 #include <stdio.h>
 
-float Average(int rows, int coloumn, int arr[rows][coloumn])
-{
-    int sum = 0;
-    int total = rows * coloumn;
-
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < coloumn; j++)
-        {
-          
-            sum += arr[i][j];
-        }
-    }
-
-    return (float)sum / total;
-}
-
 int main()
 {
-    int rows, coloumn;
+    int r, c;
+    int a[10][10];
+    int sum = 0;
 
-    printf("Enter number of rows: ");
-    scanf("%d", &rows);
+    scanf("%d %d", &r, &c);
 
-    printf("Enter number of columns: ");
-    scanf("%d", &coloumn);
-
-    int arr[rows][coloumn];
-
-    printf("Enter elements of array:\n");
-
-    for (int i = 0; i < rows; i++)
+    for (int i = 0; i < r; i++)
     {
-        for (int j = 0; j < coloumn; j++)
+        for (int j = 0; j < c; j++)
         {
-              printf("arr[%d][%d]" ,i,j);
-            scanf("%d", &arr[i][j]);
+            scanf("%d", &a[i][j]);
+            sum += a[i][j];
         }
     }
 
-    float avg = Average(rows, coloumn, arr);
+    float avg = (float)sum / (r * c);
 
-    printf("\n Average is: %.2f\n", avg);
+    printf("Average = %.2f", avg);
 
     return 0;
 }
